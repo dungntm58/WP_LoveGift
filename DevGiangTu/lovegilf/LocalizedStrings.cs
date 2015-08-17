c1f7268e5f7b16637b391ace608f82079f43ef92
TaGiang06011994 <tagiang111120@gmail.com>
2015-08-17 23:12:18 +0700

    Tao Folder Code rieng cho do nham

---
+++ DevGiangTu/lovegilf/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using lovegilf.Resources;
+
+namespace lovegilf
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
